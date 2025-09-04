c7d6a24834ec34db8436c569a73250bffe184d28
Jose Manuel <chema1987@hotmail.com>
2025-09-04 09:22:14 -0500

    Update Sale.java
    Cart and CartItem were excluded

--- src/main/java/com/adso/pos/model/Sale.java
+++ src/main/java/com/adso/pos/model/Sale.java
@@ -11,16 +11,28 @@ public class Sale {
     private Customer customer;
     private Date saleDate;
     private double totalAmount;
-    private List<CartItem> items;
+    private List<SaleItem> items;
 
-    // Constructor to create a new Sale instance.
-    public Sale(int saleId, Customer customer, Cart cart) {
+    // A constructor that takes a customer and the list of items to be sold.
+    public Sale(int saleId, Customer customer, List<SaleItem> items) {
         this.saleId = saleId;
         this.customer = customer;
         this.saleDate = new Date(); // Sets the current date.
-        this.totalAmount = cart.getTotalAmount();
-        // A deep copy of the cart items to save the state at the time of sale.
-        this.items = new ArrayList<>(cart.getItems());
+        this.items = new ArrayList<>(items);
+        this.totalAmount = calculateTotal(items);
+    }
+
+    /**
+     * Calculates the total amount for the sale from the list of items.
+     * @param items The list of SaleItem objects.
+     * @return The total amount for the sale.
+     */
+    private double calculateTotal(List<SaleItem> items) {
+        double total = 0;
+        for (SaleItem item : items) {
+            total += item.getSubtotal();
+        }
+        return total;
     }
 
     // Getters for the fields.
@@ -40,7 +52,8 @@ public class Sale {
         return totalAmount;
     }
 
-    public List<CartItem> getItems() {
+    public List<SaleItem> getItems() {
         return items;
     }
 }
+
